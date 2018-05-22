74ea7091daececf7a026c14a936fd36812f6aa05
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-22 14:20:57 +0200

    Type checking and comparison with secret

---
+++ README.md
@@ -0,0 +1,2 @@
+Getting started with the Rust Programming Language, following
+[TRPL](https://doc.rust-lang.org/book/second-edition/).
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,17 +3,25 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number");
     // Random number in the range [1; 100[, in other words 1 to 99
     let secret = rand::thread_rng().gen_range(1, 100);
-
     println!("The secret number is: {}", secret);
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("Guess: {}", guess);
+    let guess: u32 = guess.trim().parse()
+        .expect("Not a number");
+
+    match secret.cmp(&guess) {
+        Ordering::Less => println!("It's less"),
+        Ordering::Greater => println!("It's more"),
+        Ordering::Equal => println!("You won !"),
+    }
 }
