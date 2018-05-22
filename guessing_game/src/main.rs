21ae5a03a0b885160d57df9e445dfcef73cb0613
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-22 14:23:46 +0200

    Loop guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,19 +9,24 @@ use rand::Rng;
 fn main() {
     println!("Guess the number");
     // Random number in the range [1; 100[, in other words 1 to 99
-    let secret = rand::thread_rng().gen_range(1, 100);
-    println!("The secret number is: {}", secret);
+    let not_so_secret = rand::thread_rng().gen_range(1, 100);
+    println!("The not so secret number is: {}", not_so_secret);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Not a number");
+        let guess: u32 = guess.trim().parse()
+            .expect("Not a number");
 
-    match secret.cmp(&guess) {
-        Ordering::Less => println!("It's less"),
-        Ordering::Greater => println!("It's more"),
-        Ordering::Equal => println!("You won !"),
+        match not_so_secret.cmp(&guess) {
+            Ordering::Less => println!("It's less"),
+            Ordering::Greater => println!("It's more"),
+            Ordering::Equal => {
+                println!("You won !");
+                break;
+            }
+        }
     }
 }
