16bcc6acaf1679960d76fb0200e9182a76c6edc3
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-25 10:04:32 +0200

    Add sub-slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     string_slices();
     zero_index();
+    len_index();
+    full_size();
+    substring();
 }
 
 fn string_slices() {
@@ -18,5 +21,75 @@ fn string_slices() {
 fn zero_index() {
     let s = String::from("hello");
     let _slice = &s[0..2]; // same
-    let _slice = &s[..2]; // same
+    let slice = &s[..2]; // same
+    println!("{}", slice); // he
+}
+
+fn len_index() {
+    let s = String::from("hello");
+
+    let len = s.len();
+    let _slice = &s[2..len]; // same
+    let slice = &s[2..]; // same
+    println!("{}", slice); // llo
+}
+
+fn full_size() {
+    let s = String::from("hello");
+
+    let len = s.len();
+    let _slice = &s[0..len]; // same
+    let slice = &s[..]; // same
+    println!("{}", slice); // hello
+}
+
+fn substring() {
+    let hw = String::from("hello world");
+    let fw = first_word(&hw);
+    let sw = second_word(&hw);
+    println!("first word: {}", fw);
+    println!("second word: {}", sw);
+
+    let string = String::from("bonjour monde");
+    let literal = "bonjour monde";
+    let fw_string = first_word_slices(&string[..]);
+    let fw_literal_slice = first_word_slices(&literal[..]);
+    let fw_literal = first_word_slices(literal); // already a slice !
+    println!("first word: {} = {} = {}", fw_string, fw_literal_slice, fw_literal);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &letter) in bytes.iter().enumerate() {
+        if letter == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &letter) in bytes.iter().enumerate() {
+        if letter == b' ' {
+            return &s[i+1..] // second word onwards, not just the 2nd word
+        }
+    }
+
+    &s[..]
+}
+
+fn first_word_slices(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &letter) in bytes.iter().enumerate() {
+        if letter == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
